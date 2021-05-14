5d0bbff57843887e38108aa9f93c9c590a9cff8f
Jose Angel Hernandez Radilla <jhernandezr1313@alumno.ipn.mx>
2021-05-14 11:31:11 -0500

    Configuracion lista

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const puerto = process.env.PORT || 8000
+
+app.use(express.static(path.join(__dirname,'build')))
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'build','index.html'))
+})
+
+app.listen(puerto,()=>{
+    console.log(`Escuchando en el puerto ${puerto}`)
+})
