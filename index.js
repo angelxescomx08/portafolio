527d52bb55da9e4dcf88fd42289b443bf43f1758
Jose Angel Hernandez Radilla <jhernandezr1313@alumno.ipn.mx>
2021-05-16 01:09:10 -0500

    Portafolio listo

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const puerto = process.env.PORT || 8000
 
 app.use(express.static(path.join(__dirname,'build')))
 
+app.get('/pdf/CV.pdf', (req, res) => res.download(path.join(__dirname,'CV.pdf')))
+
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'build','index.html'))
 })
